aae23d804d1bbaf556f2f27efa1eedc5d4032714
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2014-04-28 17:51:23 +0200

    initial commit

---
+++ shirts.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "Shirts";
+include('includes/header.php'); ?>
+
+  <div class="section page">
+    <h1> Ola&rsquo;s Full Catalog Of Shirts </h1>
+  </div>
+
+<?php include('includes/footer.php'); ?>
---
+++ test.php
@@ -0,0 +1,10 @@
+<! DOCTYPE html>
+<html>
+<head>
+  <title>Shirts 4 Ola</title>
+</head>
+<body>
+  <h1><?php echo "Shirts 4 Mike"; ?></h1>
+  <p>The current server time is <?php echo date("H:i:s a"); ?></p>
+</body>
+</html>
