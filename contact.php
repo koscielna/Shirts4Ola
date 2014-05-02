16b2538af958a7e0f90969ed4aad880aaaa9bb2c
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2014-05-02 23:14:42 +0200

    Sending emails enabled.

--- contact.php
+++ contact.php
@@ -6,11 +6,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $message = $_POST["message"];
 
   $body = "";
-  $body = $body . "Name: " . $name . "\n";
-  $body = $body . "Email: " . $email . "\n";
+  $body = $body . "Name: " . $name . "<br>";
+  $body = $body . "Email: " . $email . "<br>";
   $body = $body . "Message: " . $message;
 
-  //TODO: Send Email
+  if ($name == "" OR $email == "" OR $message == "") {
+    echo "No field should remain blank.";
+    exit;
+  }
+
+  foreach ($_POST as $value) {
+    if (stripos($value, 'Content-Type: ') !== FALSE ) {
+      echo "Something wrong!";
+      exit;
+    }
+  }
+
+  if ($_POST["address"] != "") {
+    echo "Error in submission.";
+    exit;
+  }
+
+  require_once('includes/PHPMailer-master/class.phpmailer.php');
+  $mail = new PHPMailer();
+
+  $mail->SetFrom($email, $name);
+  $address = 'aleksandra.nabozny@gmail.com';
+  $mail->AddAddress($address, "Shirts 4 Ola");
+  $mail->Subject = "Shirts 4 Ola | " . $name;
+  $mail->MsgHTML($body);
+
+  if (!$mail->Send()) {
+    echo "Something went wrong.";
+    exit;
+  } else {
+    echo "Message sent!";
+  }
+
+
+  if (!$mail->ValidateAddress($email)) {
+    echo "You must enter valid email address.";
+  }
 
   header('Location: contact.php?status=thanks');
   exit;
@@ -59,6 +95,15 @@ include('includes/header.php'); ?>
                             </th>
                             <td>
                               <textarea name="message" id="message"></textarea>
+                            </td>
+                        </tr>
+                        <tr style="display: none; ">
+                            <th>
+                              <label for="address">Message</label>
+                            </th>
+                            <td>
+                              <textarea name="address" id="address"></textarea>
+                              <p>This should be blank.</p>
                             </td>
                         </tr>
                       </table>
